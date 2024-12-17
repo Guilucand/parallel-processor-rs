20b4bcb8c666a0e1eccf8e054932680975c80d26
Andrea Cracco <guilucand@gmail.com>
2024-12-17 14:00:20 +0200

    Fix memory tracking bug

--- src/execution_manager/memory_tracker.rs
+++ src/execution_manager/memory_tracker.rs
@@ -2,14 +2,16 @@ use crate::execution_manager::executor::AsyncExecutor;
 use crate::execution_manager::packet::PacketTrait;
 use crate::memory_data_size::MemoryDataSize;
 use dashmap::DashMap;
+use std::any::TypeId;
 use std::cmp::max;
 use std::io::stdout;
 use std::marker::PhantomData;
 use std::sync::Arc;
 
 pub struct MemoryTrackerManager {
-    packet_sizes: DashMap<*const str, ((usize, usize), usize)>,
-    executors_sizes: DashMap<*const str, ((usize, usize), usize)>,
+    packet_sizes: DashMap<TypeId, ((usize, usize), usize)>,
+    executors_sizes: DashMap<TypeId, ((usize, usize), usize)>,
+    type_names: DashMap<TypeId, &'static str>,
 }
 
 unsafe impl Sync for MemoryTrackerManager {}
@@ -20,6 +22,7 @@ impl MemoryTrackerManager {
         MemoryTrackerManager {
             packet_sizes: DashMap::new(),
             executors_sizes: DashMap::new(),
+            type_names: DashMap::new(),
         }
     }
 
@@ -31,8 +34,11 @@ impl MemoryTrackerManager {
         if packet.get_size() > 0 {
             let mut entry = self
                 .packet_sizes
-                .entry(std::any::type_name::<T>() as *const str)
+                .entry(TypeId::of::<T>())
                 .or_insert(((0, 0), 0));
+            self.type_names
+                .entry(TypeId::of::<T>())
+                .or_insert(std::any::type_name::<T>());
             entry.value_mut().0 .0 += packet.get_size();
             entry.value_mut().0 .1 += 1;
 
@@ -43,17 +49,14 @@ impl MemoryTrackerManager {
     }
     pub fn remove_queue_packet<T: PacketTrait>(&self, packet: &T) {
         if packet.get_size() > 0 {
-            let mut entry = self
-                .packet_sizes
-                .get_mut(&(std::any::type_name::<T>() as *const str))
-                .unwrap();
+            let mut entry = self.packet_sizes.get_mut(&TypeId::of::<T>()).unwrap();
             entry.value_mut().0 .0 -= packet.get_size();
             entry.value_mut().0 .1 -= 1;
         }
     }
 
-    fn get_pretty_name(ptr: *const str) -> String {
-        let string = unsafe { &*ptr };
+    fn get_pretty_name(&self, ptr: TypeId) -> String {
+        let string = self.type_names.get(&ptr).unwrap();
 
         let mut builder = String::new();
         let mut last_was_col = false;
@@ -90,7 +93,7 @@ impl MemoryTrackerManager {
         for executor in self.executors_sizes.iter() {
             crate::log_info!(
                 "\t{} ==> {:.2} with {} instances [MAX {:.2}]",
-                Self::get_pretty_name(*executor.key()),
+                self.get_pretty_name(*executor.key()),
                 MemoryDataSize::from_bytes(executor.value().0 .0),
                 executor.value().0 .1,
                 MemoryDataSize::from_bytes(executor.value().1)
@@ -101,7 +104,7 @@ impl MemoryTrackerManager {
         for packet in self.packet_sizes.iter() {
             crate::log_info!(
                 "\t{} ==> {:.2} with {} instances [MAX {:.2}]",
-                Self::get_pretty_name(*packet.key()),
+                self.get_pretty_name(*packet.key()),
                 MemoryDataSize::from_bytes(packet.value().0 .0),
                 packet.value().0 .1,
                 MemoryDataSize::from_bytes(packet.value().1)
@@ -120,7 +123,7 @@ impl<E: AsyncExecutor> Clone for MemoryTracker<E> {
     fn clone(&self) -> Self {
         self.manager
             .executors_sizes
-            .get_mut(&(std::any::type_name::<E>() as *const str))
+            .get_mut(&TypeId::of::<E>())
             .unwrap()
             .value_mut()
             .0
@@ -137,11 +140,16 @@ impl<E: AsyncExecutor> MemoryTracker<E> {
     fn new(manager: Arc<MemoryTrackerManager>) -> Self {
         manager
             .executors_sizes
-            .entry(std::any::type_name::<E>() as *const str)
+            .entry(TypeId::of::<E>())
             .or_insert(((0, 0), 0))
             .value_mut()
             .0
              .1 += 1;
+        manager
+            .type_names
+            .entry(TypeId::of::<E>())
+            .or_insert(std::any::type_name::<E>());
+
         MemoryTracker {
             manager,
             last_memory_usage: 0,
@@ -154,7 +162,7 @@ impl<E: AsyncExecutor> MemoryTracker<E> {
         let mut entry = self
             .manager
             .executors_sizes
-            .get_mut(&(std::any::type_name::<E>() as *const str))
+            .get_mut(&TypeId::of::<E>())
             .unwrap();
         entry.value_mut().0 .0 -= self.last_memory_usage;
         entry.value_mut().0 .0 += new_memory_usage;
@@ -171,7 +179,7 @@ impl<E: AsyncExecutor> Drop for MemoryTracker<E> {
         let mut entry = self
             .manager
             .executors_sizes
-            .get_mut(&(std::any::type_name::<E>() as *const str))
+            .get_mut(&TypeId::of::<E>())
             .unwrap();
         entry.value_mut().0 .0 -= self.last_memory_usage;
         entry.value_mut().0 .1 -= 1;
